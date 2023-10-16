824ae2635b548c55aaf3be62837842534361581f
Bash Elliott <bashelliott@gmail.com>
2023-10-17 10:32:04 +1100

    time for work

--- divali.mjs
+++ divali.mjs
@@ -1,12 +1,3 @@
 export function Divali( input ) {
-	let arr = input.split("");
-	let output = "";
-
-	for (let i = 0; i < arr.length - 1; i++) {
-		let flip = arr.shift();
-		arr.push(flip)
-	}
-	
-	output = arr.join("")
-	return ("\`\`\`" + output + "\`\`\`");
+	return ("\`\`\`" + input + "\`\`\`");
 }
