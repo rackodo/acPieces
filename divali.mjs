41c843223533b3fca1c0fce1ab098662fe0b664e
Bash Elliott <bashelliott@gmail.com>
2023-10-17 08:33:08 +1100

    init parser

--- divali.mjs
+++ divali.mjs
@@ -2,11 +2,13 @@ export function Divali( input ) {
 	let arr = input.split("");
 	let output = [];
 
-	for (let i = 0; i < arr.length + 1; i++) {
-		output.push(arr.join(""))
-		let flip = arr.shift();
-		arr.push(flip)
-	}
+	// for (let i = 0; i < arr.length + 1; i++) {
+	// 	output.push(arr.join(""))
+	// 	let flip = arr.shift();
+	// 	arr.push(flip)
+	// }
 
-	return output;
+	// return output;
+
+	return input;
 }
