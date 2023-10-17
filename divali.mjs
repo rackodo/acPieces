eaf9a0ae4e3d1d2f5fe7df0ffe8011887c6f021e
Bash Elliott <bashelliott@gmail.com>
2023-10-17 11:38:10 +1100

    ok i guess that didn't work

--- divali.mjs
+++ divali.mjs
@@ -1,5 +1,11 @@
 import { mparse } from "./mparse.mjs"
 
 export function Divali( input ) {
-	return mparse(input)
+	/* 
+	In honour of Markus Calveley. The gayest little bastard to ever be imagined. 
+	*/
+	let message = [];
+	message.push(input);
+	message.push("\*Brought to you by the letter B!\*")
+	return message.join("\n");
 }
