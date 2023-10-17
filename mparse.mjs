85dc376634769043e1c7613c8af83ca5f8b248e8
Bash Elliott <bashelliott@gmail.com>
2023-10-18 10:37:17 +1100

    update to push

--- mparse.mjs
+++ mparse.mjs
@@ -2,10 +2,8 @@ export function mparse( code ) {
 	/* 
 	In honour of Markus Calveley.
 	*/
-	// let message = [];
-	// message.push(code);
-	// message.push("\*Brought to you by the letter B!\*")
-	// return message.join("\n");
-	return "Howdy"
-	return code
+	let message = [];
+	message.push(code);
+	message.push("\*Brought to you by the letter B!\*")
+	return message.join("\n");
 }
