983a12cfea68636bc7ab0cca7cb894cd4cf3fd37
Bash Elliott <bashelliott@gmail.com>
2023-10-26 09:28:03 +1100

    what did you input?

--- mparse.mjs
+++ mparse.mjs
@@ -1,3 +1,20 @@
+const commandMap = {
+	'': 'rect', // Rectangle
+	1: 'oval', // Oval
+	2: 'line' // Line
+}
+
+const colourMap = {
+	0: [255, 0, 0],     // Red
+	1: [0, 0, 255],     // Blue
+	2: [0, 255, 0],     // Green
+	3: [255, 255, 0],   // Yellow
+	4: [128, 0, 255],   // Purple
+	5: [255, 128, 255], // Pink
+	6: [255, 255, 255], // White
+	7: [0, 0, 0]        // Black
+}
+
 export function mparse( code ) {
 	/* 
 	In honour of Markus Calveley.
@@ -8,8 +25,8 @@ export function mparse( code ) {
 	} else if (code == "🌈") {
 		message.push("You input 🌈!")
 	} else {
-		message.push("You didn't input ✨ or 🌈.")
+		message.push(`You didn't input ✨ or 🌈. Instead you input ${code}`)
 	}
-	message.push("\*Brought to you by the letter B!\* **Iteration 1.0**")
+	message.push("\*Brought to you by the letter B!\* **Iteration 1.1**")
 	return message.join("\n");
 }
