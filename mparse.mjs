e87319ce8c71c3b867f2ae6f88e45bce1d5b61c6
Bash Elliott <bashelliott@gmail.com>
2023-10-26 10:16:09 +1100

    add fallback

--- mparse.mjs
+++ mparse.mjs
@@ -8,12 +8,6 @@ Array.prototype.random = function () {
 	return this[Math.floor((Math.random()*this.length))];
 }
 
-let comments = [
-	"Brought to you by the letter B!",
-	"explodes u :3c",
-	"aesthetically pleasing"
-]
-
 const commandMap = {
 	'📐': 'rect', // Rectangle
 	'⭕️': 'oval', // Oval
@@ -31,14 +25,24 @@ const colourMap = {
 	'⬜️': 'white'
 }
 
+
+
+let comments = [
+	"Brought to you by the letter B!",
+	"explodes u :3c",
+	"aesthetically pleasing"
+]
+
+
+
 export function mparse( code ) {
 	let message = [];
 	if (code in commandMap) {
 		message.push(commandMap[code]);
+	} else if (code == 'help') {
+		message.push("This is a help message. Wooo?")
 	} else {
-		if (code == 'help') {
-			message.push("This is a help message. Wooo?")
-		}
+		message.push("Something's not right. :/")
 	}
 	message.push("------")
 	message.push(`\*${comments.random()}\*`)
