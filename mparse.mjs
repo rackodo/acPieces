3b0e66812869bb2569de234ea897dd84ec924aa0
Bash Elliott <bashelliott@gmail.com>
2023-10-26 10:34:13 +1100

    help menu

--- mparse.mjs
+++ mparse.mjs
@@ -8,10 +8,14 @@ Array.prototype.random = function () {
 	return this[Math.floor((Math.random()*this.length))];
 }
 
-const commandMap = {
-	'📐': 'rect', // Rectangle
-	'⭕️': 'oval', // Oval
-	'📏': 'line' // Line
+const shortCommands = {
+	'🐟': 'pond'
+}
+
+const longCommands = {
+	'📐': 'rect',
+	'⭕️': 'oval',
+	'📏': 'line'
 }
 
 const colourMap = {
@@ -25,27 +29,38 @@ const colourMap = {
 	'⬜️': 'white'
 }
 
-
-
 let comments = [
 	"Brought to you by the letter B!",
 	"explodes u :3c",
 	"aesthetically pleasing"
 ]
 
-
+let helpMenu = [
+	'**Long Commands**',
+	'📐 - Rectangle. Asks for colour, width and height.',
+	'⭕️ - Oval. Asks for colour, horizontal radius and vertical radius.',
+	'📏 - Line. Asks for colour, starting X positon, starting Y positon, ending X position, ending Y position.',
+	' ',
+	'**Short Commands**',
+	'🐟 - Pond. Splish splash :)'
+]
 
 export function mparse( code ) {
 	let message = [];
-	if (code in commandMap) {
-		message.push(commandMap[code]);
-	} else if (code == 'help') {
-		message.push("This is a help message. Wooo?")
+	if (code in longCommands) {
+		message.push(longCommands[code], ", a long command.");
+	} else if (code in shortCommands) {
+		message.push(shortCommands[code], ", a short command.")
+	}
+	else if (code == 'help') {
+		helpMenu.forEach(function(item) {
+			message.push(item)
+		})
 	} else {
 		message.push("Something's not right. :/")
 	}
 	message.push("------")
 	message.push(`\*${comments.random()}\*`)
-	message.push("**v6**")
+	message.push("**v7**")
 	return message.join("\n");
 }
